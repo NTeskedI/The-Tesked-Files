7d97977495e09e0fa79837026b3c4177c40a756f
NTeskedI <121166969+NTeskedI@users.noreply.github.com>
2023-03-24 11:55:25 +0100

    Lektion (jag fick ingen kaka)
    Added variables based on colors that Diana gave me after i said i wanted the site to reflect my personality more since it previously just was black and white. I asked a stack overflow question yesterday and i actually got helpful answers so now the glitch happens on page refresh :D
    
    I started making an svg for decoration but for the moment it isnt implemented

--- project.js
+++ project.js
@@ -16,6 +16,29 @@ document.getElementById("load-text").onmouseover = event => {
 
     if(iterations >= event.target.dataset.value.length) clearInterval(interval);
 
-    iterations += 1 / 3;
+    iterations += 1 / 6;
 }, 30);
 }
+
+window.onload = function () {
+    const symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+    const target = document.getElementById("load-text");
+    let iterations = 0;
+  
+    const interval = setInterval(() => {
+      target.innerText = target.innerText.split("")
+        .map((letter, index) => {
+          if (index < iterations) {
+            return target.dataset.value[index];
+          }
+  
+          return symbols[Math.floor(Math.random() * symbols.length)];
+        })
+        .join("");
+  
+      if (iterations >= target.dataset.value.length) clearInterval(interval);
+  
+      iterations += 1 / 6;
+    }, 30);
+  };
+
